6722b67ed6c18a0a1a730833ac141bea193dd8e2
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-01 15:44:07 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,6 +7,6 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-converted_input = "5".to_i
-return converted_input 
+converted_input = "#{user_input}".to_i
+return converted_input - 1
 end
